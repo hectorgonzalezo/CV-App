eeb91cc31452ce3c99c96574e98a5ac42a66e352
Hector Gonzalez <gorozco.hector@gmail.com>
2022-10-02 17:52:17 -0400

    Fix export  to pdf

--- src/components/CV.js
+++ src/components/CV.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import InputField from "./InputField";
 import InputMultiple from './InputMultiple';
 import Picture from './Picture';
-import html2PDF from 'jspdf-html2canvas';
+import html2pdf from 'html2pdf.js'
 
 class CV extends Component {
   constructor() {
@@ -17,18 +17,19 @@ class CV extends Component {
   
     async exportToPDF(){
        const cvDiv = this.cvRef.current;
-       const height = window.getComputedStyle(cvDiv).height.match(/\d+/)[0];
-       const width = window.getComputedStyle(cvDiv).width.match(/\d+/)[0];
-       console.log(height, width)
-       // Pass height and width of cv div as sizes for pdf
-       await html2PDF(cvDiv, {
-        jsPDF: {
-          format: [width, height],
-        },
-        image:{ type: 'jpeg', quality: 1 },
-        imageType: 'image/jpeg',
-        output: './pdf/CV.pdf'
-      })
+       const height = Number(window.getComputedStyle(cvDiv).height.match(/\d+/)[0]);
+       const width = Number(window.getComputedStyle(cvDiv).width.match(/\d+/)[0]);
+       console.log({height, width})
+    //    Pass height and width of cv div as sizes for pdf
+    var opt = {
+        filename:     'CV.pdf',
+        image:        { type: 'jpeg', quality: 1 },
+        html2canvas:  { scale: 1 , width, height},
+        jsPDF:        { unit: 'px', format: [width, height], hotfixes: ["px_scaling"]}
+      };
+    
+    // choose the element and pass it to html2pdf() function and call the save() on it to save as pdf.
+    html2pdf().set(opt).from(cvDiv).save();
     }
 
   clickSubmit(e) {
